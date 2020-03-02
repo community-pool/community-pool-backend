0a006e8b9dc16034e80a9b49da24f0bae866b838
Jack McConnell <jackmc1122@gmail.com>
2020-03-01 21:23:16 -0800

    Add cors support

--- lib/app.ts
+++ lib/app.ts
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+// import * as cors from 'cors';
+const cors = require('cors');
+app.use(
+  cors({
+    origin: true,
+    credentials: true
+  })
+);
 
 app.use(express.json());
 
