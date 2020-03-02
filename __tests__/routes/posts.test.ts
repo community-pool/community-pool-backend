bb3809edda97aa59ca6db03707359eedf7ad59f1
Jack McConnell <jackmc1122@gmail.com>
2020-03-01 16:11:10 -0800

    Add a get posts route

--- __tests__/routes/posts.test.ts
+++ __tests__/routes/posts.test.ts
@@ -4,6 +4,8 @@ import * as request from 'supertest';
 import { app } from '../../lib/app';
 import { connect } from '../../lib/utils/connect';
 import * as mongoose from 'mongoose';
+import Post from '../../lib/models/Post';
+import { createPosts } from '../helpers/create-posts';
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -29,6 +31,21 @@ describe('app routes', () => {
             userName: 'jack',
             title: 'testTitle',
             content: 'testConent'
+          });
+        });
+    });
+
+    it('Returns a created post', async () => {
+      await createPosts();
+      return request(app)
+        .get('/api/v1/posts')
+        .then(res => {
+          expect(res.body).toHaveLength(3);
+          expect(res.body[0]).toEqual({
+            _id: expect.any(String),
+            userName: 'jack',
+            title: 'jackTitle',
+            content: 'jackContent'
           });
         });
     });
--- lib/routes/posts.ts
+++ lib/routes/posts.ts
@@ -1,7 +1,11 @@
 const { Router } = require('express');
 import Post from '../models/Post';
 
-module.exports = Router().post('/', (req, res, next) => {
-  const { userName, title, content } = req.body;
-  Post.create({ userName, title, content }).then(post => res.send(post));
-});
+module.exports = Router()
+  .get('/', (req, res, next) => {
+    Post.find().then(posts => res.send(posts));
+  })
+  .post('/', (req, res, next) => {
+    const { userName, title, content } = req.body;
+    Post.create({ userName, title, content }).then(post => res.send(post));
+  });
