594ea40771a608c6cba9fa56888cb4319ec6ccb3
Jack McConnell <jackmc1122@gmail.com>
2020-03-03 19:32:41 -0800

    Bring back the test

--- lib/__tests__/routes/posts.test.ts
+++ lib/__tests__/routes/posts.test.ts
@@ -35,121 +35,121 @@ describe('app routes', () => {
       });
   });
 
-  // it('Returns a new posts', () => {
-  //   return request(app)
-  //     .post('/api/v1/posts')
-  //     .send({ userName: 'jack', content: 'testConent' })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         status: 400,
-  //         message: 'Post validation failed: title: Path `title` is required.'
-  //       });
-  //     });
-  // });
+  it('Returns a new posts', () => {
+    return request(app)
+      .post('/api/v1/posts')
+      .send({ userName: 'jack', content: 'testConent' })
+      .then(res => {
+        expect(res.body).toEqual({
+          status: 400,
+          message: 'Post validation failed: title: Path `title` is required.'
+        });
+      });
+  });
 
-  // it('Returns a list of posts', async () => {
-  //   const posts = await createPosts();
-  //   return request(app)
-  //     .get('/api/v1/posts')
-  //     .then(res => {
-  //       expect(res.body).toHaveLength(3);
-  //       expect(res.body[0]).toEqual({
-  //         _id: expect.any(String),
-  //         userName: posts[0].userName,
-  //         title: posts[0].title,
-  //         content: posts[0].content
-  //       });
-  //     });
-  // });
+  it('Returns a list of posts', async () => {
+    const posts = await createPosts();
+    return request(app)
+      .get('/api/v1/posts')
+      .then(res => {
+        expect(res.body).toHaveLength(3);
+        expect(res.body[0]).toEqual({
+          _id: expect.any(String),
+          userName: posts[0].userName,
+          title: posts[0].title,
+          content: posts[0].content
+        });
+      });
+  });
 
-  // it('Returns a specific post', async () => {
-  //   const posts = await createPosts();
-  //   return request(app)
-  //     .get(`/api/v1/posts/${posts[2]._id}`)
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.any(String),
-  //         userName: posts[2].userName,
-  //         title: posts[2].title,
-  //         content: posts[2].content
-  //       });
-  //     });
-  // });
+  it('Returns a specific post', async () => {
+    const posts = await createPosts();
+    return request(app)
+      .get(`/api/v1/posts/${posts[2]._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          userName: posts[2].userName,
+          title: posts[2].title,
+          content: posts[2].content
+        });
+      });
+  });
 
-  // it('Returns a deleted post', async () => {
-  //   const posts = await createPosts();
-  //   return request(app)
-  //     .delete(`/api/v1/posts/${posts[0]._id}`)
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.any(String),
-  //         userName: 'jack',
-  //         title: 'jackTitle',
-  //         content: 'jackContent'
-  //       });
+  it('Returns a deleted post', async () => {
+    const posts = await createPosts();
+    return request(app)
+      .delete(`/api/v1/posts/${posts[0]._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          userName: 'jack',
+          title: 'jackTitle',
+          content: 'jackContent'
+        });
 
-  //       return request(app).get(`/api/v1/posts/${posts[0]._id}`);
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({ status: 404, message: 'Not Found' });
-  //     });
-  // });
+        return request(app).get(`/api/v1/posts/${posts[0]._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({ status: 404, message: 'Not Found' });
+      });
+  });
 
-  // it('Fails to delete a post that is already deleted', async () => {
-  //   const posts = await createPosts();
-  //   return request(app)
-  //     .delete(`/api/v1/posts/${posts[0]._id}`)
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.any(String),
-  //         userName: 'jack',
-  //         title: 'jackTitle',
-  //         content: 'jackContent'
-  //       });
+  it('Fails to delete a post that is already deleted', async () => {
+    const posts = await createPosts();
+    return request(app)
+      .delete(`/api/v1/posts/${posts[0]._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          userName: 'jack',
+          title: 'jackTitle',
+          content: 'jackContent'
+        });
 
-  //       return request(app).delete(`/api/v1/posts/${posts[0]._id}`);
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({ status: 404, message: 'Not Found' });
-  //     });
-  // });
+        return request(app).delete(`/api/v1/posts/${posts[0]._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({ status: 404, message: 'Not Found' });
+      });
+  });
 
-  // it('Updates a post', async () => {
-  //   const posts = await createPosts();
-  //   return request(app)
-  //     .put(`/api/v1/posts/${posts[0]._id}`)
-  //     .send({ title: 'newTitle', content: 'newContent' })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.any(String),
-  //         userName: 'jack',
-  //         title: 'newTitle',
-  //         content: 'newContent'
-  //       });
-  //     });
-  // });
+  it('Updates a post', async () => {
+    const posts = await createPosts();
+    return request(app)
+      .put(`/api/v1/posts/${posts[0]._id}`)
+      .send({ title: 'newTitle', content: 'newContent' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          userName: 'jack',
+          title: 'newTitle',
+          content: 'newContent'
+        });
+      });
+  });
 
-  // it('Fails to Update a post', async () => {
-  //   const posts = await createPosts();
-  //   return request(app)
-  //     .delete(`/api/v1/posts/${posts[0]._id}`)
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.any(String),
-  //         userName: 'jack',
-  //         title: 'jackTitle',
-  //         content: 'jackContent'
-  //       });
+  it('Fails to Update a post', async () => {
+    const posts = await createPosts();
+    return request(app)
+      .delete(`/api/v1/posts/${posts[0]._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          userName: 'jack',
+          title: 'jackTitle',
+          content: 'jackContent'
+        });
 
-  //       return request(app)
-  //         .put(`/api/v1/posts/${posts[0]._id}`)
-  //         .send({ title: 'newTitle', content: 'newContent' });
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         message: 'Not Found',
-  //         status: 404
-  //       });
-  //     });
-  // });
+        return request(app)
+          .put(`/api/v1/posts/${posts[0]._id}`)
+          .send({ title: 'newTitle', content: 'newContent' });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          message: 'Not Found',
+          status: 404
+        });
+      });
+  });
 });
