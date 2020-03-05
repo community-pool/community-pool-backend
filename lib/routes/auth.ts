0d0e05268ef40ef834ee7b5fe9d574dc6c775d4e
Jose <j.a.ojeda@live.com>
2020-03-04 20:52:52 -0800

    wrote /callback route in auth.ts

--- lib/routes/auth.ts
+++ lib/routes/auth.ts
@@ -10,10 +10,26 @@
 
  require('dotenv').config();
 
- module.exports = Router().get('/login', passport.authenticate('auth0', {
-   scope: 'openid email profile'
- }),
- (req, res) => {
+ module.exports = Router()
+ .get('/login', passport.authenticate('auth0', { scope: 'openid email profile' }), (req, res) => {
    res.redirect('/');
- }
- );
+ })
+
+ .get('/callback', (req, res, next) => {
+   passport.authenticate('auth0', (err, user, info) => {
+     if(err) {
+       return next(err);
+     }
+     if(!user) {
+       return res.redirect('/login');
+     }
+     req.logIn(user, (err) => {
+       if(err) {
+         return next(err);
+       }
+       const returnTo = req.session.returnTo;
+       delete req.session.returnTo;
+       res.redirect(returnTo || '/')
+     });
+   })(req, res, next);
+ });
